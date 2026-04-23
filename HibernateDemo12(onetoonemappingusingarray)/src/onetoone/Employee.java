3b12ede329e4af09a59c4aecbff2dfd1bdf434bb
Krishana <Krishana@DESKTOP-V9B2BNL>
2026-04-23 22:13:23 +0530

    my practice

---
+++ HibernateDemo12(onetoonemappingusingarray)/src/onetoone/Employee.java
@@ -0,0 +1,15 @@
+package onetoone;
+
+public class Employee {
+	public static void main(String[] args) {
+		 int a = 987;
+		 
+		 if(a%2==0) {
+			 System.out.println("it is event number");
+		 }
+		 else {
+			 System.out.println("it is odd number");
+		 }
+	}
+
+}
