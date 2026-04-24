43de89db4c31c04cde6410f197a754e7ee599143
Krishana <Krishana@DESKTOP-V9B2BNL>
2026-04-24 23:40:36 +0530

    Implement dayfinder with switch case condition

---
+++ HibernateDemo12(onetoonemappingusingarray)/src/dayfinder/dayginderswitchcase.java
@@ -0,0 +1,45 @@
+package dayfinder;
+
+import java.util.Scanner;
+
+public class dayginderswitchcase {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number here : ");
+		int input = sc.nextInt();
+		
+		switch (input) {
+		case 1:{
+			System.out.println("monday");
+		}break;
+		case 2 :{
+			System.out.println("tuesday");
+		}break;
+		case 3 :{
+			System.out.println("wednesday");
+			
+		}break;
+		case 4 :{
+			System.out.println("thursday");
+		}break;
+		case 5 :{
+			
+			System.out.println("friday");
+		}break;
+		case 6 : {
+			System.out.println("saturday");
+		}break;
+		case 7 :{
+			System.out.println("sunday");
+		}break;	
+		default:
+			System.err.println("invalid credential)");
+		}
+		
+		
+		
+	}
+
+}
